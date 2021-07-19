404c78578493590d2a726402b32feae89c850526
Andrei <andrei.barbu@travelperk.com>
2021-07-19 14:00:09 +0200

    finish app functionality

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Recipe.css';
 
-function Recipe(props) {
+const Recipe = props => {
     let recipe = props.recipe;
-    return (
-        <div className="recipe">
-            <p>Recipe name: {recipe.name}</p>
-            <p>Recipe description: {recipe.description}</p>
-            <p>Ingredients: &nbsp;
-                {recipe.ingredients.map((val, idx) => {
-                    if (idx+1===recipe.ingredients.length) {
-                        return val.name;
-                    } else {
-                        return val.name+", ";
-                    }
-                })}
-            </p>
-        </div>
-    );
+    let [isEditing, setIsEditing] = useState(false);
+
+    const getInputValue = inputId => {
+        if (document.querySelector("#" + inputId).value) {
+            return document.querySelector("#" + inputId).value;
+        } else {
+            return recipe[inputId];
+        }
+    }
+
+    const handleDelete = () => {
+        props.remove(recipe.id);
+    };
+
+    const handleEdit = () => {
+        console.log("Creating payload..");
+        let ingredients = recipe.ingredients;
+        if (document.querySelector("#ingredients").value) {
+            ingredients = [];
+            const ingredientsArray = getInputValue("ingredients").split(",");
+            ingredientsArray.map((val) => ingredients.push({ "name": val }));
+        }
+        const payload = {
+            id: recipe.id,
+            name: getInputValue("name"),
+            description: getInputValue("description"),
+            ingredients: ingredients,
+        }
+        props.edit(recipe.id, payload);
+        console.log(payload);
+        setIsEditing(false);
+    }
+
+    if (isEditing) {
+        return (
+            <div className="recipe form">
+                <input type="text" name="name" id="name" placeholder="name" />
+                <input type="text" name="description" id="description" placeholder="description" />
+                <p>Ingredients: (Separate with comma)</p><br />
+                <textarea name="ingredients" id="ingredients" cols="30" rows="4"></textarea><br />
+                <div className="buttons">
+                    <button onClick={handleEdit}>Done</button>
+                    <button onClick={(() => setIsEditing(false))}>Cancel</button>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="recipe">
+                <p className="name">{recipe.name}</p>
+                <p className="description">{recipe.description}</p>
+                <div className="ingredients">{recipe.ingredients.map((val, idx) => {
+                    return <span className="ingredient" key={idx}>{val.name}</span>
+                })}</div>
+                <div className="buttons">
+                    <button onClick={handleDelete}>Delete</button>
+                    <button onClick={(() => setIsEditing(true))}>Edit</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Recipe;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -2,19 +2,22 @@ import React, { useState, useEffect } from 'react';
 import Recipe from './Recipe';
 import './Recipe.css';
 
-function RecipeList() {
+const RecipeList = () => {
+    // State
     let [recipes, setRecipes] = useState({});
-    let [haveData, setHaveData] = useState(false)
+    let [haveData, setHaveData] = useState(false);
+    let [formShown, setFormShown] = useState(false);
+
+    // Fetch all recipes from server
     useEffect(() => {
-        async function getData() {
-            let data;
+        const getData = async () => {
             await fetch("http://0.0.0.0:8000/recipes/", {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
                 },
             })
-                .then((response) => data = response.json())
+                .then((response) => response.json())
                 .then((data) => {
                     setRecipes(data);
                     setHaveData(true);
@@ -23,6 +26,85 @@ function RecipeList() {
         getData();
     }, [haveData]);
 
+    // Add a recipe
+    const newRecipe = async (e) => {
+        e.preventDefault();
+        let ingredients = [];
+        const ingredientsArray = document.querySelector("#ingredients").value.split(",");
+        ingredientsArray.map((val) => ingredients.push({ "name": val }));
+        const payload = {
+            name: document.querySelector("#name").value,
+            description: document.querySelector("#description").value,
+            ingredients: ingredients,
+        }
+        console.log(payload);
+        console.log("Sending POST...")
+        await fetch("http://0.0.0.0:8000/recipes/", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload)
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                    let updatedRecipes = [...recipes];
+                    updatedRecipes.push(data);
+                    setRecipes(updatedRecipes);
+                    document.querySelector("form").reset();
+                    setFormShown(false);
+                });
+    }
+
+    // Edit a recipe
+    const editRecipe = async (recipeId, payload) => {
+
+        const changeRecipeDetails = () => {
+            let interimRecipes = [...recipes];
+            let index = recipes.findIndex(recipe => recipe.id === recipeId);
+            interimRecipes[index] = payload;
+            setRecipes(interimRecipes);
+        }
+        console.log(JSON.stringify(payload));
+        await fetch("http://0.0.0.0:8000/recipes/" + recipeId + "/", {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload)
+        })
+            .then((response) => {
+                console.log("Status: " + response.status + " " + response.statusText);
+                if (response.status === 200) {
+                    changeRecipeDetails();
+                } else {
+                    alert("Error editing recipe");
+                }
+            });
+    }
+
+    // Remove a recipe
+    const removeRecipe = async (recipeId) => {
+        console.log("Sending delete request...")
+        await fetch("http://0.0.0.0:8000/recipes/" + recipeId + "/", {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                console.log("Status: " + response.status + " " + response.statusText);
+                if (response.status === 204) {
+                    const updatedRecipes = recipes.filter(recipe => recipe.id !== recipeId);
+                    setRecipes(updatedRecipes);
+                } else {
+                    alert("Error deleting recipe");
+                }
+            });
+    }
+
+    // Render
     if (!haveData) {
         return (
             <h1>Loading...</h1>
@@ -30,10 +112,26 @@ function RecipeList() {
     } else {
         return (
             <div className="recipe-list">
-                <h1>Get your recipes</h1>
-                {haveData ? (recipes.map((val) => {
-                    return <Recipe recipe={val} key={val.id} />;
-                })):''}
+                <h1>Your recipes</h1>
+
+                <button onClick={(() => setFormShown(true))}>Add new</button>
+
+                {recipes.map((val) => {
+                    return <Recipe recipe={val} key={val.id} remove={removeRecipe} edit={editRecipe} />;
+                })}
+
+                {formShown ? (
+                    <form className="recipe form" onSubmit={(e) => newRecipe(e)}>
+                        <input type="text" name="name" id="name" placeholder="name" required />
+                        <input type="text" name="description" id="description" placeholder="description" required />
+                        <p>Ingredients: (Separate with comma)</p><br />
+                        <textarea name="ingredients" id="ingredients" cols="30" rows="4" required></textarea><br />
+                        <div className="buttons">
+                            <input type="submit" />
+                            <button onClick={(() => setFormShown(false))}>Cancel</button>
+                        </div>
+                    </form>
+                ) : ""}
             </div>
         );
     }
